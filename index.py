5bae3d4767b26465a2e79a668f8f346fd4c3559f
Majid <majid.benmerah@outlook.com>
2025-03-31 17:43:42 +0200

    Question 2 à 7

--- index.py
+++ index.py
@@ -1,12 +1,79 @@
 import streamlit as st
-from src.mongodb import getGetYearWithMostMovies
+from src.mongodb import getGetYearWithMostMovies, countMoviesAfter1999, averageVotes2007, getMoviesCountPerYear, getAllGenres, getHighestRevenueMovie, getDirectorsWithMoreThan5Movies
+import matplotlib.pyplot as plt
 
 st.title("Projet MongoDB - Analyse de films")
-st.subheader("Année avec le plus de films sorties :")
+
+#Question 1
+st.subheader("1. Afficher l’année où le plus grand nombre de films ont été sortis.")
 
 yearInfo = getGetYearWithMostMovies()
 
-if yearInfo:
-    st.write(f"**{yearInfo['_id']}** avec **{yearInfo['count']}** films.")
+st.write(f"**{yearInfo['_id']}** avec **{yearInfo['count']}** films.")
+st.write("Pas trouv")
+
+
+# Question 2
+count = countMoviesAfter1999()
+st.subheader("2. Quel est le nombre de films sortis après l’année 1999.")
+st.write(f"Il y a **{count}** films sorties après 1999.")
+
+
+#Question 3
+votes = averageVotes2007()
+st.subheader("3. Quelle est la moyenne des votes des films sortis en 2007.")
+st.write(f"la moyenne des votes est **{votes}**.")
+
+
+#Question4
+st.subheader("4. Affichez un histogramme qui permet de visualiser le nombres de films par année.")
+data = getMoviesCountPerYear()
+
+anneee = [item['_id'] for item in data if item['_id'] is not None]
+counts = [item['count'] for item in data if item['_id'] is not None]
+
+plt.figure(figsize=(10, 4))
+plt.bar(anneee, counts)
+plt.xlabel("Année")
+plt.ylabel("Nombre de films")
+plt.title("Nombre de films par année")
+st.pyplot(plt)
+
+#Question 5
+st.subheader("5. Quelles sont les genres de films disponibles dans la bases")
+
+genres = getAllGenres()
+
+st.write(f"Il y a **{len(genres)}** genres .")
+st.write(", ".join(genres))
+
+
+# Question 6
+st.subheader("6. Quel est le film qui a généré le plus de revenu.")
+
+movie = getHighestRevenueMovie()
+st.write(f"**{movie['title']}** a généré **{movie['Revenue (Millions)']} millions**.")
+
+# Question 7
+st.subheader("7. Quels sont les réalisateurs ayant réalisé plus de 5 films dans la base de données ?")
+
+directors = getDirectorsWithMoreThan5Movies()
+if directors:
+    for d in directors:
+        st.write(f"- {d['_id']} : {d['count']} films")
 else:
-    st.write("Pas trouv")
+    st.write(f"Aucun")
+
+
+# Question 8
+st.subheader("8. Quel est le genre de film qui rapporte en moyenne le plus de revenus ?")
+
+# Question 9
+st.subheader("9. Quels sont les 3 films les mieux notés (rating) pour chaque décennie (1990-1999, 2000-2009,etc.) ?")
+
+
+# Question 10
+st.subheader("10. Quel est le film le plus long (Runtime) par genre ?")
+
+# Question 11
+st.subheader("11. Créer une vue MongoDB affichant uniquement les films ayant une note supérieure à 80 (Metascore) et généré plus de 50 millions de dollars.")
--- src/mongodb.py
+++ src/mongodb.py
@@ -6,6 +6,7 @@ def getCollection():
     database = client[MONGO_DB_NAME]
     return database[MONGO_COLLECTION_NAME]
 
+# Question 1
 def getGetYearWithMostMovies():
     collection = getCollection()
     pipeline = [
@@ -15,3 +16,62 @@ def getGetYearWithMostMovies():
     ]
     result = list(collection.aggregate(pipeline))
     return result[0] if result else None
+
+# Question 2
+def countMoviesAfter1999():
+    collection = getCollection()
+    return collection.count_documents({"year": {"$gt": 1999}})
+
+# Question3
+def averageVotes2007():
+    collection = getCollection()
+    pipeline = [
+        {"$match": {"year": 2007}},
+        {"$group": {"_id": None, "avg_votes": {"$avg": "$Votes"}}}
+    ]
+    result = list(collection.aggregate(pipeline))
+    return result[0]["avg_votes"] if result else None
+
+#Question4
+def getMoviesCountPerYear():
+    collection = getCollection()
+    pipeline = [
+        {"$group": {"_id": "$year", "count": {"$sum": 1}}},
+        {"$sort": {"_id": 1}}
+    ]
+    return list(collection.aggregate(pipeline))
+
+
+#Question 5
+def getAllGenres():
+    collection = getCollection()
+    pipeline = [
+        {"$project": {"genre": {"$split": ["$genre", ","]}}},
+        {"$unwind": "$genre"},
+        {"$group": {"_id": "$genre"}},
+        {"$sort": {"_id": 1}}
+    ]
+    result = list(collection.aggregate(pipeline))
+    return [doc["_id"].strip() for doc in result]
+
+
+# Question 6
+def getHighestRevenueMovie():
+    collection = getCollection()
+    pipeline = [
+        {"$match": {"Revenue (Millions)": {"$type": "double"}}},
+        {"$sort": {"Revenue (Millions)": -1}},
+        {"$limit": 1}
+    ]
+    result = list(collection.aggregate(pipeline))
+    return result[0] if result else None
+
+# Question 7
+def getDirectorsWithMoreThan5Movies():
+    collection = getCollection()
+    pipeline = [
+        {"$group": {"_id": "$Director", "count": {"$sum": 1}}},
+        {"$match": {"count": {"$gt": 5}}},
+        {"$sort": {"count": -1}}
+    ]
+    return list(collection.aggregate(pipeline))
